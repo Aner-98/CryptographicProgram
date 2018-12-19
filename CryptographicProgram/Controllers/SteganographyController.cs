ac9ab2b44436c40426e605f2b028001e370b09ed
Yurii Fedelesh <yurii.fedelesh@gmail.com>
2018-12-19 11:53:58 +0200

    Update controller actions, fix bugs

--- CryptographicProgram/Controllers/SteganographyController.cs
+++ CryptographicProgram/Controllers/SteganographyController.cs
@@ -1,34 +1,48 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
 using CryptographicProgram.Algorithms.Abstractions;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Hosting;
+
 
 namespace CryptographicProgram.Controllers
 {
 	[Route("api/[controller]")]
-    [ApiController]
-    public class SteganographyController : ControllerBase
-    {
-	    private string _imageName => HomeController.ImagePath;
+	[ApiController]
+	public class SteganographyController : ControllerBase
+	{
 		private readonly ISteganographyAlgorithm _steganographyAlgorithm;
+		private readonly IHostingEnvironment _appEnvironment;
 
-	    public SteganographyController(ISteganographyAlgorithm steganographyAlgorithm)
-	    {
-		    _steganographyAlgorithm = steganographyAlgorithm;
-	    }
 
-	    [HttpGet]
-	    [Route("GetEncode")]
-	    public IActionResult EncodeText([FromQuery]string text)
-	    {
-		    _steganographyAlgorithm.Encode(new Bitmap(_imageName), text).Save(_imageName);
-			return Ok();
-	    }
+		public SteganographyController(ISteganographyAlgorithm steganographyAlgorithm, IHostingEnvironment appEnvironment)
+		{
+			_appEnvironment = appEnvironment;
+			_steganographyAlgorithm = steganographyAlgorithm;
+		}
 
-	    [HttpGet]
-	    [Route("SecretText")]
-	    public IActionResult GetSecretText()
-	    {
-		    return Ok(_steganographyAlgorithm.Decode(new Bitmap(_imageName)));
-	    }
+		[HttpGet]
+		[Route("GetEncode")]
+		public IActionResult EncodeText([FromQuery]string text)
+		{
+			var path = _appEnvironment.WebRootPath + HomeController.ImagePath;
+			var bitmap = _steganographyAlgorithm.Encode(new Bitmap(path), text);
+
+			var fileName = Guid.NewGuid().ToString() + ".bmp";
+			var filePath = _appEnvironment.WebRootPath + @"\images\" + fileName;
+
+			bitmap.Save(filePath, System.Drawing.Imaging.ImageFormat.Bmp);
+			HomeController.ImagePath = "/images/" + fileName;
+
+			var image = System.IO.File.OpenRead(filePath);
+			return File(image, "image/jpeg");
+		}
+
+		[HttpGet]
+		[Route("SecretText")]
+		public IActionResult GetSecretText()
+		{
+			return Ok(new { text = _steganographyAlgorithm.Decode(new Bitmap(_appEnvironment.WebRootPath + HomeController.ImagePath)) });
+		}
 	}
 }
