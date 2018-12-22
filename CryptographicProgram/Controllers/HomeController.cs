015201769eca9cb2e9955cf595e653aae4e7a28e
Yurii Fedelesh <yurii.fedelesh@gmail.com>
2018-12-22 22:59:08 +0200

    Delete usings

--- CryptographicProgram/Controllers/HomeController.cs
+++ CryptographicProgram/Controllers/HomeController.cs
@@ -1,5 +1,4 @@
-﻿using System.Diagnostics;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Hosting;
