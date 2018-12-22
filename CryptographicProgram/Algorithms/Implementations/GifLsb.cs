479d03f5b1454303cb69a23b6fc4a960d945c6f6
Yurii Fedelesh <yurii.fedelesh@gmail.com>
2018-12-22 22:54:43 +0200

    Add class for new algorithm

---
+++ CryptographicProgram/Algorithms/Implementations/GifLsb.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Drawing;
+using CryptographicProgram.Algorithms.Abstractions;
+
+namespace CryptographicProgram.Algorithms.Implementations
+{
+	public class GifLsb : ISteganographyAlgorithm
+	{
+		public Bitmap Encode(Bitmap bitmap, string text)
+		{
+			throw new NotImplementedException();
+		}
+
+		public string Decode(Bitmap bitmap)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
